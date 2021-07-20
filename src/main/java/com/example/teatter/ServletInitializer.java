babde284bd00c8afd0940974e2c85fa56839c82d
Oleksandr Haleta <alexhaleta99@gmail.com>
2021-07-20 10:07:44 +0300

    Initial commit

---
+++ src/main/java/com/example/teatter/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.teatter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TeatterApplication.class);
+	}
+
+}
---
+++ src/main/java/com/example/teatter/TeatterApplication.java
@@ -0,0 +1,13 @@
+package com.example.teatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeatterApplication.class, args);
+	}
+
+}
